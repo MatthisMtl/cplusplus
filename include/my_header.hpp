7dea44a6c7e00fed1878e5a83395a489a140c676
matthis <matthis.monteil@gmail.com>
2018-01-31 14:43:05 +0100

    j'ai fini les exos

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,17 +1,19 @@
-//  Copyright (c) 2017 Antoine Tran Tan
-//
+//  copyright (c) 2018 Antoine Tran Tan
+//  copyright (c) 2018 Matthis Monteil
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
-
-
-
-#endif // MY_HEADER_HPP
-
 double exo1(double rayon)
 {
 	double aire;
 	aire = 3.14*rayon*rayon;
 	return aire;
 }
-#endif
+
+void exo2(short a, short b)
+{
+	short y;
+	y = a < b && !a;
+}
+#endif // MY_HEADER_HPP
+
--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
 //  Copyright (c) 2018 Antoine Tran Tan
-//
+//  Copyright (c) 2018 Matthis Monteil
 
 #include <iostream>
 #include <my_header.hpp>
@@ -8,6 +8,8 @@ int main()
 {
     // Affichage sur un terminal
     std::cout << "Hello World!" << std::endl;
-
+	exo2(5, 10);
+	exo2(0, 10);
+	exo2(20, 10);
     return 0;
 }
