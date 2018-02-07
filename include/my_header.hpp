b02dfaa7ff4605c70a4242ccef3c2fed1666977d
matthis <eleve@si-PC01>
2018-02-07 14:58:54 +0100

    nous avons fini l'exo 2

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -13,6 +13,19 @@ void exo1(void)
 		c = c + 1;
 	}
 }
+void exo2(void)
+{
+	long n = 3, fac = 1, i = 1;
 
+	if (n > 0)
+		while (i <= 1)
+		{
+			fac = fac * i;
+			i = i + 1;
+		}
+	else
+		if (n < 0)
+			fac = 0;
+}
 #endif // MY_HEADER_HPP
 
--- source/main.cpp
+++ source/main.cpp
@@ -8,8 +8,9 @@ int main()
 {
     // Affichage sur un terminal
     std::cout << "Hello World!" << std::endl;
-	exo2(5, 10);
-	exo2(0, 10);
-	exo2(20, 10);
-    return 0;
+ 	exo2();
+	
+	return 0;
+
 }
+
